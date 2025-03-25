d7cfadf9e7737c32e81ee0fb4a841e7e0d44443c
RubyFoster <Ruby@SaturnNet.com.au>
2025-03-26 10:02:55 +1100

    ASCII Art Change

--- source/lv2/asciiart.h
+++ source/lv2/asciiart.h
@@ -19,7 +19,8 @@ static char* asciiart = "\n"
 	" SSSSSSSSSSSSSSS     aaaaaaaaaa  aaaa         ttttttttttt      uuuuuuuu  uuuurrrrrrr              nnnnnn    nnnnnn             CCCCCCCCCCCCC   ooooooooooo     nnnnnn    nnnnnn  sssssssssss       ooooooooooo   llllllll    eeeeeeeeeeeeee    sssssssssss    \n"
 	"             Xenon Linux Loader Reloaded v0.993, Saturn Edition" "\n";
 static char* asciitail =
-    "    Greetings to: Octal450, JustAnyone, Juan, InvoxiPlayGames, GrimDoomer\n\n"
+    "    Greetings to: Octal450, JustAnyone, Juan, InvoxiPlayGames, GrimDoomer, Swizzy, 15432, MrMario2011 Project Free60\n\n"
 	"    Fuck You MTW\n\n"
 	"    Rest in Peace Jimmy, you were a dear friend, We won't forget what you did for us\n\n"
+	"    Pimps At Sea Lives Forever\n\n"
 	"    Special Corona & Winchester Compatible XeLL version\n\n";

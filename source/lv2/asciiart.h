d98036f9134a3a93c3d36f0bd7673bcc87dfb91a
RubyFoster <Ruby@SaturnNet.com.au>
2025-03-26 12:02:43 +1100

    Patch 02

--- source/lv2/asciiart.h
+++ source/lv2/asciiart.h
@@ -1,15 +1,16 @@
 static char* asciiart = "\n"
-	"███████  █████  ████████ ██    ██ ██████  ███    ██      ██████  ██████  ███    ██ ███████  ██████  ██      ███████ ███████ \n"
-	"██      ██   ██    ██    ██    ██ ██   ██ ████   ██     ██      ██    ██ ████   ██ ██      ██    ██ ██      ██      ██      \n"
-	"███████ ███████    ██    ██    ██ ██████  ██ ██  ██     ██      ██    ██ ██ ██  ██ ███████ ██    ██ ██      █████   ███████ \n"
-	"     ██ ██   ██    ██    ██    ██ ██   ██ ██  ██ ██     ██      ██    ██ ██  ██ ██      ██ ██    ██ ██      ██           ██ \n"
-	"███████ ██   ██    ██     ██████  ██   ██ ██   ████      ██████  ██████  ██   ████ ███████  ██████  ███████ ███████ ███████ \n"
+"   _____       _                       _____                      _           \n"
+"  / ____|     | |                     / ____|                    | |          \n"
+" | (___   __ _| |_ _   _ _ __ _ __   | |     ___  _ __  ___  ___ | | ___  ___ \n"
+"  \___ \ / _` | __| | | | '__| '_ \  | |    / _ \| '_ \/ __|/ _ \| |/ _ \/ __|\n"
+"  ____) | (_| | |_| |_| | |  | | | | | |___| (_) | | | \__ \ (_) | |  __/\__ \\n"
+" |_____/ \__,_|\__|\__,_|_|  |_| |_|  \_____\___/|_| |_|___/\___/|_|\___||___/\n"
     "             Xenon Linux Loader Reloaded v0.993, Saturn Edition\n";
 
 static char* asciitail =
-    "    Special Corona & Winchester Compatible XeLL version\n\n"
+    "    Special Corona & Winchester Compatible XeLL version\n\n";
     "    Presenting the Beth Special\n"
     "    Greetings to: Octal450, JustAnyone, Juan, InvoxiPlayGames, GrimDoomer, Swizzy, 15432, MrMario2011 Project Free60\n"
     "    Fuck You MTW\n"
     "    Rest in Peace Jimmy, you were a dear friend, We won't forget what you did for us\n"
-    "    Pimps At Sea Lives Forever\n";
+    "    Pimps At Sea Lives Forever\n\n";

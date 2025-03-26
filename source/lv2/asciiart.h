5b9ed9104dec3cb1b80f6de72d214e491435a2e1
RubyFoster <Ruby@SaturnNet.com.au>
2025-03-26 12:12:43 +1100

    Art Patch 9000

--- source/lv2/asciiart.h
+++ source/lv2/asciiart.h
@@ -14,13 +14,13 @@ static char* asciiart = "\n"
 "  SSSSSSS     S:::::S   a::::a    a:::::a         t::::::tttt:::::t   u:::::::::::::::uu   r:::::r                 n::::n    n::::n \n"
 "  S::::::SSSSSS:::::S   a:::::aaaa::::::a         tt::::::::::::::t    u:::::::::::::::u   r:::::r                 n::::n    n::::n \n"
 "  S:::::::::::::::SS     a::::::::::aa:::a          tt:::::::::::tt     uu::::::::uu:::u   r:::::r                 n::::n    n::::n \n"
-"   SSSSSSSSSSSSSSS        aaaaaaaaaa  aaaa            ttttttttttt         uuuuuuuu  uuuu   rrrrrrr                 nnnnnn    nnnnnn \n\n";
-    "             Xenon Linux Loader Reloaded v0.993, Saturn Edition\n";
+"   SSSSSSSSSSSSSSS        aaaaaaaaaa  aaaa            ttttttttttt         uuuuuuuu  uuuu   rrrrrrr                 nnnnnn    nnnnnn \n\n"
+"             Xenon Linux Loader Reloaded v0.993, Saturn Edition\n";
 
 static char* asciitail =
-    "    Special Corona & Winchester Compatible XeLL version\n\n";
-    "    Presenting the Beth Special\n"
-    "    Greetings to: Octal450, JustAnyone, Juan, InvoxiPlayGames, GrimDoomer, Swizzy, 15432, MrMario2011 Project Free60\n"
-    "    Fuck You MTW\n"
-    "    Rest in Peace Jimmy, you were a dear friend, We won't forget what you did for us\n"
-    "    Pimps At Sea Lives Forever\n\n";
+"    Special Corona & Winchester Compatible XeLL version\n\n"
+"    Presenting the Beth Special\n"
+"    Greetings to: Octal450, JustAnyone, Juan, InvoxiPlayGames, GrimDoomer, Swizzy, 15432, MrMario2011 Project Free60\n"
+"    Fuck You MTW\n"
+"    Rest in Peace Jimmy, you were a dear friend, We won't forget what you did for us\n"
+"    Pimps At Sea Lives Forever\n\n";

c46be6d1878e9f2eb96d67c77c04c2fcf95a9360
RubyFoster <Ruby@SaturnNet.com.au>
2025-03-26 11:52:28 +1100

    ASCII Patch

--- source/lv2/asciiart.h
+++ source/lv2/asciiart.h
@@ -8,7 +8,7 @@ static char* asciiart = "\n"
 static char* asciitail =
     "    Special Corona & Winchester Compatible XeLL version\n\n";
 	"    Presenting the Beth Special\n";
-    "    Greetings to: Octal450, JustAnyone, Juan, InvoxiPlayGames, GrimDoomer, Swizzy, 15432, MrMario2011 Project Free60\n"
-	"    Fuck You MTW\n"
-	"    Rest in Peace Jimmy, you were a dear friend, We won't forget what you did for us\n"
-	"    Pimps At Sea Lives Forever\n"
+    "    Greetings to: Octal450, JustAnyone, Juan, InvoxiPlayGames, GrimDoomer, Swizzy, 15432, MrMario2011 Project Free60\n";
+	"    Fuck You MTW\n";
+	"    Rest in Peace Jimmy, you were a dear friend, We won't forget what you did for us\n";
+	"    Pimps At Sea Lives Forever\n";
